35c645dc06d146cac84142aa161c0ae30d7ef450
Pulkit Kaushik <pulkit.kaushik@finoit.com>
2017-04-11 11:47:13 +0530

    Master commit on git

--- GitTesting/ViewController.swift
+++ GitTesting/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func buttonClicked(_ sender: UIButton) {
-        print("This is a Git tutorial")
+        print("You finished!")
     }
 }
 
